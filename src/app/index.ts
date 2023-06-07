00d2ffd3c20eb683058c5fbae3a6a78d16445828
Yadheedhya06 <Yadheedhya6@gmail.com>
2023-06-07 13:53:15 +0530

    added tool

---
+++ src/app/index.ts
@@ -0,0 +1,44 @@
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { OpenAI } from "langchain/llms/openai";
+import {
+    JsonListKeysTool,
+    JsonGetValueTool,
+    RequestsGetTool,
+} from "langchain/tools";
+import { BigQueryTool } from "./tools/BigQueryTool";
+
+const model = new OpenAI({
+    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY || '',
+    modelName: "gpt-3.5-turbo",
+});
+
+class CustomRequestGetTool extends RequestsGetTool {
+    description =
+        "An api request proxy, Use this when you need to get specific content from an api. Input should be a url mentioned before.";
+}
+
+async function main() {
+    const tools = [
+        /* await AIPluginTool.fromPluginUrl(
+          "https://plugin-ai.vercel.app/.well-known/ai-plugin.json"
+        ),
+        new CustomRequestGetTool(), */
+        new BigQueryTool(),
+    ];
+
+    const executor = await initializeAgentExecutorWithOptions(tools, model, {
+        agentType: "zero-shot-react-description",
+        verbose: true,
+        maxIterations: 10, // Only fixed iterations are allowed so agent don't go crazy
+    });
+
+    const result = await executor.call({
+        input:
+            "list of top 10 ethereum addresses ordered by balance. convert to simple list",
+    });
+
+    console.log(`🗿 Output: ${result.output}`);
+}
+
+main();
